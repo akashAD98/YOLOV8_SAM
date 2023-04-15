a8fa83c61f82372d66c2ae789bfe36b2b6353fa4
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2023-04-15 22:07:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,67 +1,39 @@
-import cv2
-import matplotlib.pyplot as plt
-import numpy as np
 from ultralytics import YOLO
+import numpy as np
+import cv2
+import sys
+sys.path.append("..")
 from segment_anything import sam_model_registry, SamPredictor
+import matplotlib.pyplot as plt
 
-from PIL import Image
+def yolov8_detection():
 
-
-def yolov8_detection(image):
-    model = YOLO('/content/best.pt')
+    model=YOLO('/content/best_adam_lr_batch16_az.pt')
+    image = "/content/Cigaretee_i_Weapon_Pragati138.jpg"
+    
     results = model(image, stream=True)  # generator of Results objects
+
     for result in results:
         boxes = result.boxes  # Boxes object for bbox outputs
-    bbox = boxes.xyxy.tolist()[0]
+    
+    bbox=boxes.xyxy.tolist()[0]
     return bbox
 
+image = cv2.imread('/content/Cigaretee_i_Weapon_Pragati138.jpg')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-def show_mask(mask, ax, random_color=False):
-    if random_color:
-        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
-    else:
-        color = np.array([30/255, 144/255, 255/255, 0.6])
-    h, w = mask.shape[-2:]
-    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
-    ax.imshow(mask_image)
-
-
-def show_points(coords, labels, ax, marker_size=375):
-    pos_points = coords[labels==1]
-    neg_points = coords[labels==0]
-    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
-    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
-
-
-def show_box(box, ax):
-    x0, y0 = box[0], box[1]
-    w, h = box[2] - box[0], box[3] - box[1]
-    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))
-
-
-# Load image
-# image_path = '/content/myimg.jpg'
-# image = cv2.imread(image_path)
-# image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-
-# Load image
-image_path = '/content/myimage.jpg'
-image = Image.open(image_path).convert('RGB')
-
-
-# Detection with YOLOv5
-yolov8_boxex = yolov8_detection(image)
-input_box = np.array(yolov8_boxex)
-
-# Segment with SAM
 sam_checkpoint = "sam_vit_h_4b8939.pth"
 model_type = "vit_h"
 device = "cpu"
+
 sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
 sam.to(device=device)
+
 predictor = SamPredictor(sam)
 predictor.set_image(image)
+
+yolov8_boxex = yolov8_detection()
+input_box = np.array(yolov8_boxex)
 
 masks, _, _ = predictor.predict(
     point_coords=None,
@@ -70,10 +42,32 @@ masks, _, _ = predictor.predict(
     multimask_output=False,
 )
 
-# Show the image with the mask and box overlaid
-plt.figure(figsize=(10, 10))
-plt.imshow(image)
-show_mask(masks[0], plt.gca())
-show_box(input_box, plt.gca())
-plt.axis('off')
-plt.show()
+for i, mask in enumerate(masks):
+    # Convert the mask to a binary image
+    #binary_mask = mask.cpu().numpy().squeeze().astype(np.uint8)
+    # Convert the mask to a binary image    
+    binary_mask = torch.from_numpy(masks).squeeze().numpy().astype(np.uint8)
+
+
+    # Find the contours of the mask
+    contours, hierarchy = cv2.findContours(binary_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Get the largest contour based on area
+    largest_contour = max(contours, key=cv2.contourArea)
+
+    # Get the new bounding box
+    bbox = [int(x) for x in cv2.boundingRect(largest_contour)]
+
+    # Get the segmentation mask for object 
+    segmentation = largest_contour.flatten().tolist()
+
+    plt.figure(figsize=(10, 10))
+    plt.imshow(image)
+    show_mask(masks[0], plt.gca())
+    show_box(input_box, plt.gca())
+    plt.axis('off')
+    plt.show()
+
+    # Print the bounding box and segmentation mask
+    print("Bounding box:", bbox)
+    print("Segmentation mask:", segmentation)
