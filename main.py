7b4e1bc99266466d0783d132375446e533f0fef8
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2023-04-15 09:52:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,21 @@
-from ultralytics import YOLO
-import torch
-import torchvision
-    
-import numpy as np
-import torch
+import cv2
 import matplotlib.pyplot as plt
-import cv2
-
-
-print("PyTorch version:", torch.__version__)
-print("Torchvision version:", torchvision.__version__)
-print("CUDA is available:", torch.cuda.is_available())
-    
-# download weights files  
-import sys
-!{sys.executable} -m pip install opencv-python matplotlib
-!{sys.executable} -m pip install 'git+https://github.com/facebookresearch/segment-anything.git'
-
-!mkdir images
-!wget -P images https://raw.githubusercontent.com/facebookresearch/segment-anything/main/notebooks/images/truck.jpg
-!wget -P images https://raw.githubusercontent.com/facebookresearch/segment-anything/main/notebooks/images/groceries.jpg        
-!wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
-
-    
-import cv2
-image = cv2.imread('/content/Cigaretee_i_Weapon_Pragati138.jpg')
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-
-import sys
-sys.path.append("..")
+import numpy as np
+from ultralytics import YOLO
 from segment_anything import sam_model_registry, SamPredictor
 
+from PIL import Image
 
-sam_checkpoint = "sam_vit_h_4b8939.pth"
-model_type = "vit_h"
-device = "cpu"
-sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
-sam.to(device=device)
-predictor = SamPredictor(sam)
-predictor.set_image(image)
 
-def yolov8_detection():
-
-    model=YOLO('yolov8s.pt')
-    results = model("/content/Cigaretee_i_Weapon_Pragati138.jpg", stream=True)  # generator of Results objects
-
+def yolov8_detection(image):
+    model = YOLO('/content/best.pt')
+    results = model(image, stream=True)  # generator of Results objects
     for result in results:
         boxes = result.boxes  # Boxes object for bbox outputs
-    
-    bbox=boxes.xyxy.tolist()[0]
+    bbox = boxes.xyxy.tolist()[0]
     return bbox
- 
+
+
 def show_mask(mask, ax, random_color=False):
     if random_color:
         color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
@@ -60,31 +24,56 @@ def show_mask(mask, ax, random_color=False):
     h, w = mask.shape[-2:]
     mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
     ax.imshow(mask_image)
-    
+
+
 def show_points(coords, labels, ax, marker_size=375):
     pos_points = coords[labels==1]
     neg_points = coords[labels==0]
     ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
-    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)   
-    
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+
+
 def show_box(box, ax):
     x0, y0 = box[0], box[1]
     w, h = box[2] - box[0], box[3] - box[1]
-    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))    
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))
 
-yolov8_boxex=yolov8_detection()
+
+# Load image
+# image_path = '/content/Cigaretee_i_Weapon_Pragati138.jpg'
+# image = cv2.imread(image_path)
+# image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+
+# Load image
+image_path = '/content/Cigaretee_i_Weapon_Pragati138.jpg'
+image = Image.open(image_path).convert('RGB')
+
+
+# Detection with YOLOv5
+yolov8_boxex = yolov8_detection(image)
+input_box = np.array(yolov8_boxex)
+
+# Segment with SAM
+sam_checkpoint = "sam_vit_h_4b8939.pth"
+model_type = "vit_h"
+device = "cpu"
+sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+sam.to(device=device)
+predictor = SamPredictor(sam)
+predictor.set_image(image)
 
 masks, _, _ = predictor.predict(
     point_coords=None,
     point_labels=None,
-    box=yolov8_boxex,
+    box=input_box[None, :],
     multimask_output=False,
 )
 
+# Show the image with the mask and box overlaid
 plt.figure(figsize=(10, 10))
 plt.imshow(image)
 show_mask(masks[0], plt.gca())
 show_box(input_box, plt.gca())
 plt.axis('off')
 plt.show()
-  
