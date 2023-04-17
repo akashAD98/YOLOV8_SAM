fc456eb5f34baefcf96c85d06a2161c95c9352ba
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2023-04-17 14:31:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ sys.path.append("..")
 from segment_anything import sam_model_registry, SamPredictor
 import matplotlib.pyplot as plt
 
-def yolov8_detection():
+def yolov8_detection(model, image_path):
 
-    model=YOLO('/content/best_adam_lr_batch16_az.pt')
-    image = "/content/Cigaretee_i_Weapon_Pragati138.jpg"
-    
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = model(image, stream=True)  # generator of Results objects
 
     for result in results:
         boxes = result.boxes  # Boxes object for bbox outputs
     
     bbox=boxes.xyxy.tolist()[0]
-    return bbox
+    return bbox, image
 
-image = cv2.imread('/content/Cigaretee_i_Weapon_Pragati138.jpg')
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+image_path = '/content/Cigaretee_i_Weapon_Pragati137.jpg'
+model=YOLO('/content/best.pt')
+yolov8_boxex, image = yolov8_detection(model, image_path)
 
 sam_checkpoint = "sam_vit_h_4b8939.pth"
 model_type = "vit_h"
@@ -32,7 +32,6 @@ sam.to(device=device)
 predictor = SamPredictor(sam)
 predictor.set_image(image)
 
-yolov8_boxex = yolov8_detection()
 input_box = np.array(yolov8_boxex)
 
 masks, _, _ = predictor.predict(
@@ -43,11 +42,9 @@ masks, _, _ = predictor.predict(
 )
 
 for i, mask in enumerate(masks):
-    # Convert the mask to a binary image
-    #binary_mask = mask.cpu().numpy().squeeze().astype(np.uint8)
     # Convert the mask to a binary image    
+    #binary_mask = mask.cpu().numpy().squeeze().astype(np.uint8)
     binary_mask = torch.from_numpy(masks).squeeze().numpy().astype(np.uint8)
-
 
     # Find the contours of the mask
     contours, hierarchy = cv2.findContours(binary_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -60,6 +57,41 @@ for i, mask in enumerate(masks):
 
     # Get the segmentation mask for object 
     segmentation = largest_contour.flatten().tolist()
+
+    # Write bounding boxes to file in YOLO format
+    with open('bounding_boxe_NEW_137.txt', 'w') as f:
+        for contour in contours:
+            # Get the bounding box coordinates of the contour
+            x, y, w, h = cv2.boundingRect(contour)
+            # Convert the coordinates to YOLO format and write to file
+            f.write('0 {:.6f} {:.6f} {:.6f} {:.6f}\n'.format((x+w/2)/image.shape[1], (y+h/2)/image.shape[0], w/image.shape[1], h/image.shape[0]))
+
+    mask=segmentation
+    
+    # load the image
+    #width, height = image_path.size
+    img = Image.open("/content/Cigaretee_i_Weapon_Pragati137.jpg")
+    width, height = img.size
+
+    # convert mask to numpy array of shape (N,2)
+    mask = np.array(mask).reshape(-1,2)
+
+    # normalize the pixel coordinates
+    mask_norm = mask / np.array([width, height])
+
+    # compute the bounding box
+    xmin, ymin = mask_norm.min(axis=0)
+    xmax, ymax = mask_norm.max(axis=0)
+    bbox_norm = np.array([xmin, ymin, xmax, ymax])
+
+    # concatenate bbox and mask to obtain YOLO format
+    yolo = np.concatenate([bbox_norm, mask_norm.reshape(-1)])
+
+    # write the yolo values to a text file
+    with open('yolo_cigaNEW_137_MASKw.txt', 'w') as f:
+        for val in yolo:
+            f.write("{:.6f} ".format(val))
+
 
     plt.figure(figsize=(10, 10))
     plt.imshow(image)
